3b36699a291f739cf430f35c7b26042bfcb7d04b
Sulaiman <sulaiman.saleh1100@gmail.com>
2023-03-19 23:07:39 +0700

    Add Footer & navbar

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,12 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Navbar from '../components/navbar'
+import Footer from '../components/footer'
+
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <Navbar />
         <Main />
         <NextScript />
+        <Footer />
       </body>
     </Html>
   )
